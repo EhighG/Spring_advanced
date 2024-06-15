f1c1a52dab16b1a21a02ba2cc7dcec585e2af7e1
Ehigh <rkd2rb@naver.com>
2024-06-15 14:51:18 +0900

    test: 동시성 문제 재현 테스트코드 추가

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = new Runnable() {
+            @Override
+            public void run() {
+                fieldService.logic("userB");
+            }
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 X
+        sleep(100); // 동시성 문제 O
+        threadB.start();
+        /* 여기서 끝내면, logic().sleep()이 실행된 시점에 테스트를 끝내버린다.
+        (block된 시점에 메인 쓰레드(=field()의 쓰레드)가 이어서 실행되고, 메소드가 끝난다.)
+         */
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
