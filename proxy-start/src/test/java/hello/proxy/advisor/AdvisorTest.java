df8bf147e22f2c8478128ab209f44b86c463d21a
Ehigh <rkd2rb@naver.com>
2024-06-23 01:00:16 +0900

    test: 프록시 팩토리 사용 예제 추가
    - target이 인터페이스 기반인지, 구현 클래스인지 구분해서
      jdk 동적 프록시, CGLIB 중 적합한 것을 자동 생성
    - proxyFactory.proxyTargetClass 옵션을 true로 설정 시,
      항상 CGLIB으로 생성
    - Advisor(Advice + Pointcut)를 구현 혹은 선택하여, proxyFactory에 세팅하는 방식으로 사용
    - Advice는 기존 InvocationHandler와 MethodInterceptor의 단계를 추상화한
      개념으로, (프록시로 적용할) 한 기능 단위 로직을 담는 역할

---
+++ proxy-start/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,109 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1() {
+        ServiceInterface target = new ServiceImpl();
+        // 어떤 타겟에 프록시를 적용할지를 설정
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        // advisor 생성, 적용
+        /* 어느 로직을 어디에 적용할지를 설정
+        Pointcut.TRUE = 항상 참을 반환하는 Pointcut.
+        아래는 포인트컷 없이 addAdvice(advice) 만 했을 때 결과적으로 실행되는 코드이다.
+        ProxyFactory는 어드바이저가 필수이다.
+         */
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        // 어떤 타겟에 프록시를 적용할지를 설정
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        // 직접 만든 포인트컷 기반 어드바이저 생성
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷") // 이외에 aspectJ가 제공하는 수많은 포인트컷들이 있다. (AspectJExpressionPointcut)
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        // 어떤 타겟에 프록시를 적용할지를 설정
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        // 스프링 제공 포인트컷 기반 어드바이저 생성
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("save");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인트컷 호출 methodName = {}, targetClass = {}", method.getName(), targetClass);
+            log.info("포인트컷 결과 = {}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+}
