7d1109c0781c1cfe740f5f90ff4db733ea9315f3
Ehigh <rkd2rb@naver.com>
2024-06-24 00:13:26 +0900

    refactor: 로깅Advisor 포인트컷 메소드명 -> aspectJ 표현식 방식으로 대체
    - aspectJ 표현식을 사용함으로써 더 디테일한 설정이 가능해짐

--- proxy-start/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy-start/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,7 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+//    @Bean // advisor1, 2 모두 빈으로 등록하면, 한 ThreadLocal 안에서 두 개 모두 작동하므로 동시성 이슈 때처럼 동작한다.
     public Advisor advisor1(LogTrace logTrace) {
         // pointcut ('no-log' 메소드는 제외하기 위함)
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
@@ -25,6 +26,28 @@ public class AutoProxyConfig {
         // advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         // pointcut을 기반으로 빈 클래스마다 모든 메소드를 검사해서 한 메소드라도 대상인 클래스는, 프록시 객체를 대신 등록한다.
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        // pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))"); // 모든 반환 타입, app 하위 패키지, 모든 메소드 이름, 파라미터는 상관없이
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        // pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
 }
