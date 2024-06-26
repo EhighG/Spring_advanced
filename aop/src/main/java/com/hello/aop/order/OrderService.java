88426f62406d15cf00bcded9b8123575f2699438
Ehigh <rkd2rb@naver.com>
2024-06-26 17:53:28 +0900

    test: 어드바이스 적용 시점 예제 추가

--- aop/src/main/java/com/hello/aop/order/OrderService.java
+++ aop/src/main/java/com/hello/aop/order/OrderService.java
@@ -3,6 +3,9 @@ package com.hello.aop.order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Service
 public class OrderService {
--- aop/src/test/java/com/hello/aop/AopTest.java
+++ aop/src/test/java/com/hello/aop/AopTest.java
@@ -11,13 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Slf4j
 @SpringBootTest
 //@Import(AspectV1.class) // 주로 설정 파일을 추가할 때 사용한다. 스프링 빈으로 등록할 수 있다.
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class}) // 각각 등록해야 한다.
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class}) // 각각 등록해야 한다.
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
