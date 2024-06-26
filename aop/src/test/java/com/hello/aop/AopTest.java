83845c90e0481b6f46a46b984fa0ed8c67212235
Ehigh <rkd2rb@naver.com>
2024-06-26 15:32:20 +0900

    test: 간단한 스프링 AOP 사용 예제 추가

---
+++ aop/src/test/java/com/hello/aop/AopTest.java
@@ -0,0 +1,41 @@
+package com.hello.aop;
+
+import com.hello.aop.order.OrderRepository;
+import com.hello.aop.order.OrderService;
+import com.hello.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class) // 주로 설정 파일을 추가할 때 사용한다. 스프링 빈으로 등록할 수 있다.
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
